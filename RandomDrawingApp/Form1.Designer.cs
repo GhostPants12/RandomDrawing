cda63f9cc20991d7360ddabef819e9118dc2798e
GhostPants12 <hitlerwasdoubleright@gmail.com>
2021-02-02 18:05:17 +0300

    Initial commit

---
+++ RandomDrawingApp/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace RandomDrawingApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.width = new System.Windows.Forms.TextBox();
+            this.height = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Message = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(57, 313);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(220, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Width";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Height";
+            // 
+            // width
+            // 
+            this.width.Location = new System.Drawing.Point(67, 12);
+            this.width.Name = "width";
+            this.width.Size = new System.Drawing.Size(243, 26);
+            this.width.TabIndex = 3;
+            // 
+            // height
+            // 
+            this.height.Location = new System.Drawing.Point(67, 60);
+            this.height.Name = "height";
+            this.height.Size = new System.Drawing.Size(243, 26);
+            this.height.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(128, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Message";
+            // 
+            // Message
+            // 
+            this.Message.Location = new System.Drawing.Point(12, 114);
+            this.Message.Name = "Message";
+            this.Message.Size = new System.Drawing.Size(298, 193);
+            this.Message.TabIndex = 6;
+            this.Message.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(322, 372);
+            this.Controls.Add(this.Message);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.height);
+            this.Controls.Add(this.width);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "RandomImageApp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox width;
+        private System.Windows.Forms.TextBox height;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox Message;
+    }
+}
+
---
+++ RandomDrawingApp/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomDrawingApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int imageWidth;
+            int imageHeight;
+            if(!int.TryParse(this.width.Text, out imageWidth) || !int.TryParse(this.height.Text, out imageHeight))
+            {
+                MessageBox.Show("Неверный формат разрешения (не число).", "Ошибка", MessageBoxButtons.OK);
+                return;
+            }
+
+            using(var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+                using(var bitmap = new Bitmap(imageWidth, imageHeight))
+                {
+                    if (!this.Message.Text.Equals(string.Empty))
+                    {
+                        DrawMessageBitmap(this.Message.Text, bitmap);
+                    }
+                    else 
+                    {
+                        DrawRandomBitmap(bitmap);
+                    }
+
+                    if(result == DialogResult.OK)
+                    {
+                        bitmap.Save(@fbd.SelectedPath + "\\random.png", ImageFormat.Png);
+                    }
+                }
+            }
+        }
+
+        private void DrawRandomBitmap(Bitmap bitmap)
+        {
+            Brush[] brushArr = { Brushes.Yellow, Brushes.Blue, Brushes.Green, Brushes.Red};
+            Random rnd = new Random();
+            int buf;
+            using(Graphics g = Graphics.FromImage(bitmap))
+            {
+                for(int x = 0; x<bitmap.Width; x++)
+                {
+                    for(int y =0; y<bitmap.Height;y++)
+                    {
+                        buf = rnd.Next(0,4);
+                        g.FillRectangle(brushArr[buf], x, y, 1, 1);
+                    }
+                }
+            }
+        }
+
+        private void DrawMessageBitmap(string message, Bitmap bitmap)
+        {
+            if(bitmap.Height*bitmap.Width < message.Length)
+            {
+                MessageBox.Show("Слишком маленькое разрешение.", "Ошибка", MessageBoxButtons.OK);
+                return;
+            }
+
+            int index = 0;
+            List<SolidBrush> brushes = new List<SolidBrush>();
+            Random rnd = new Random();
+            int randomResult, unicodeIndex, modPart, divPart;
+            foreach (char character in message)
+            {
+                randomResult = rnd.Next(0,255);
+                unicodeIndex = character;
+                modPart = unicodeIndex % 255;
+                divPart = (unicodeIndex/255)%255;
+                if(divPart == 0)
+                {
+                    divPart = Math.Abs(randomResult-modPart);
+                }
+                if(randomResult<127)
+                {
+                    brushes.Add(new SolidBrush(Color.FromArgb(modPart, divPart, randomResult)));
+                }
+                else
+                {
+                    brushes.Add(new SolidBrush(Color.FromArgb(divPart, modPart, randomResult)));
+                }
+            }
+
+            using(Graphics g = Graphics.FromImage(bitmap))
+            {
+                for(int x = 0; x < bitmap.Width; x++)
+                {
+                    for(int y = 0; y < bitmap.Height; y++)
+                    {
+                        if(index < brushes.Count)
+                        {
+                            g.FillRectangle(brushes[index], x, y, 1, 1);
+                            index++;
+                        }
+                        else
+                        {
+                            g.FillRectangle(Brushes.White, x, y, 1, 1);
+                            index=0;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
